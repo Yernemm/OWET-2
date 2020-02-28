47cf15f1cab3a9bd06393b12964e30e981a7678b
Yernemm <yernemm@yernemm.xyz>
2020-02-28 19:37:56 +0000

    E

--- src/js/DTUpdater.js
+++ src/js/DTUpdater.js
@@ -1,15 +1,40 @@
 const Downloader = require('./Downloader.js');
-class DTUpdater
-{
+const https = require('https');
+class DTUpdater {
     //https://ci.appveyor.com/api/projects/yretenai/owlib/branch/master
-//https://ci.appveyor.com/api/buildjobs/[JOB-ID]/artifacts/dist%2Ftoolchain-release.zip
-    DTUpdater(){
+    //https://ci.appveyor.com/api/buildjobs/[JOB-ID]/artifacts/dist%2Ftoolchain-release.zip
+    DTUpdater() {
 
     }
 
-    downloadDT(){
+    downloadDT() {
+        console.log('piss n shit')
+        return new Promise((reslove, reject) => {
+            
+            const options = {
+                hostname: 'ci.appveyor.com',
+                path: '/api/projects/yretenai/owlib/branch/master',
+                method: 'GET'
+            };
+
+            const req = https.request(options, res => {
+                console.log(`statusCode: ${res.statusCode}`);
+
+                res.setEncoding('utf8');
+
+                res.once('data', d => {
+                    console.log(JSON.parse(d).build.jobs[0].jobId);
+                });
+            });
+
+            req.on('error', error => {
+                console.error(error);
+            });
+
+            req.end();
+        });
         let jobId;
-        let dtDown = new Downloader(`https://ci.appveyor.com/api/buildjobs/${jobId}/artifacts/dist%2Ftoolchain-release.zip`,'toolchain.zip','temp');
+        let dtDown = new Downloader(`https://ci.appveyor.com/api/buildjobs/${jobId}/artifacts/dist%2Ftoolchain-release.zip`, 'toolchain.zip', 'temp');
     }
 }
 
